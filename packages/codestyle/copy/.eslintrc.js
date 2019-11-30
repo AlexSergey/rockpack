0cb5e9f1b8b9a465cec499cc7a169e89f22ba1fb
Sergey Aleksandrov <gooddev.sergey@gmail.com>
2019-11-30 13:08:44 +0200

    - update codestyle

--- packages/codestyle/copy/.eslintrc.js
+++ packages/codestyle/copy/.eslintrc.js
@@ -57,27 +57,20 @@ module.exports = extend(
             'react/prefer-stateless-function': 0,
             'react/destructuring-assignment': 0,
             'react/jsx-one-expression-per-line': 0,
-            'react/jsx-props-no-multi-spaces': 0, // this rule doesn't comprehend generics in JSX
-            'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
+            'react/jsx-props-no-multi-spaces': 0,
             'prefer-destructuring': 0,
             'promise/no-nesting': 0,
             // basic overrides
-            'comma-dangle': 0, // prettier takes care of this and can't be made to cooperate here..
+            'comma-dangle': 0,
             'no-use-before-define': 0,
             'no-underscore-dangle': 0,
             'class-methods-use-this': 0,
             'import/no-unresolved': 0,
-            'import/no-extraneous-dependencies': [
-                'error',
-                { devDependencies: ['**/*.test.{ts,tsx}', '**/test-utils/**/*.{ts,tsx}'] },
-            ],
-            'no-shadow': 'off',
-            'no-unused-vars': 'off',
-            'max-len': [2, { code: 120, ignorePattern: '^import\\W.*' }],
-            // typescript-eslint overrides
+            'no-shadow': 0,
+            'no-unused-vars': 0,
             '@typescript-eslint/array-type': 1,
             '@typescript-eslint/explicit-function-return-type': [
-                'warn',
+                1,
                 {
                     allowExpressions: true,
                     allowTypedFunctionExpressions: true,
@@ -85,7 +78,7 @@ module.exports = extend(
             ],
             '@typescript-eslint/explicit-member-accessibility': [1, { accessibility: 'no-public' }],
             '@typescript-eslint/member-delimiter-style': 1,
-            '@typescript-eslint/no-non-null-assertion': 1, // TODO: change this back to an error ASAP
+            '@typescript-eslint/no-non-null-assertion': 1,
             '@typescript-eslint/no-use-before-define': 1,
             '@typescript-eslint/no-var-requires': 1,
             '@typescript-eslint/type-annotation-spacing': 1,
