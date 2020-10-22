8b8766124f7db1af977b6cf6648c457930a7bcc6
Sergey Aleksandrov <saleksandrov@griddynamics.com>
2020-10-22 09:06:21 +0300

    add arguments

--- packages/starter/bin/rockpack.js
+++ packages/starter/bin/rockpack.js
@@ -4,11 +4,31 @@ const { getCurrentPath } = require('../utils/pathes');
 const install = require('../lib/install');
 const packageJSON = require('../package.json');
 const latestVersion = require('latest-version');
+const yargs = require('yargs');
 
 (async () => {
-  const args = process.argv.slice(2);
+  const { _, h, help, v, version } = yargs.argv;
+  const noName = _.length === 0;
 
-  if (args.length === 0) {
+  if (v || version) {
+    console.log(`Rockpack v${chalk.green(packageJSON.version)}`);
+    process.exit();
+  }
+
+  if (h || help) {
+    console.log(chalk.bold('USAGE'));
+    console.log(`  ${chalk.underline('rockpack')} proj`);
+    console.log();
+    console.log(chalk.bold('ARGUMENTS'));
+    console.log(`  ${chalk.green('<project-name>')}           Project name`);
+    console.log();
+    console.log(chalk.bold('GLOBAL OPTIONS'));
+    console.log(`  ${chalk.green('-h')} (--help)              Display this help message`);
+    console.log(`  ${chalk.green('-v')} (--version)           Display this application version`);
+    process.exit();
+  }
+
+  if (noName) {
     console.error('Please specify the project directory:');
     console.log(
       `  rockpack ${chalk.green('<project-directory>')}`
@@ -18,6 +38,8 @@ const latestVersion = require('latest-version');
     console.log(`  rockpack ${chalk.green('project-name')}`);
     process.exit(1);
   }
+
+  const name = _[0];
 
   const rockpackLatestVersion = await latestVersion(packageJSON.name);
 
