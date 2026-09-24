7cedf374a31d5318f4423f5191c8d6fab926aaa6
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2026-09-24 10:39:08 +0200

    feat(tsconfig): add a DOM-free tsconfig.node.json and drop stray base options (Plan 2 D3)

--- packages/utils/src/polyfills/text-encoder.fix.ts
+++ packages/utils/src/polyfills/text-encoder.fix.ts
@@ -1,5 +1,4 @@
 import { TextDecoder, TextEncoder } from 'node:util';
 
 global.TextEncoder = TextEncoder;
-// The shared tsconfig includes the DOM lib, whose TextDecoder also accepts SharedArrayBuffer; drop with Plan 2 D3.
-global.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
+global.TextDecoder = TextDecoder;
