66b1f6917ee659aa6d60ecb6b70a7e773d1030da
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2026-09-24 10:33:01 +0200

    chore: hoist tsx, typescript and @types/node 24 to the root (Plan 2 C7, D2)

--- packages/utils/src/polyfills/text-encoder.fix.ts
+++ packages/utils/src/polyfills/text-encoder.fix.ts
@@ -1,4 +1,5 @@
 import { TextDecoder, TextEncoder } from 'node:util';
 
 global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+// The shared tsconfig includes the DOM lib, whose TextDecoder also accepts SharedArrayBuffer; drop with Plan 2 D3.
+global.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
